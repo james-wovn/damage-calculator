2ea3de53b12268a9fc341b6eec7b87f7dd2f6620
jbwovn <james.ball@wovn.io>
2021-02-23 15:15:24 +0000

    Output average resolution across 10000 repetitions.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import statistics
 from typing import List, TypedDict
 
 
@@ -14,6 +15,13 @@ class UnitProfile(TypedDict, total=False):
     ballistic_skill: int
     toughness: int
     save: int
+
+
+class Result(TypedDict):
+    average_hits: float
+    average_wounds: float
+    average_saves: float
+    average_damage: float
 
 
 class Die(object):
@@ -33,49 +41,50 @@ class Die(object):
         self._total = random.randint(1, self.faces)
 
 
-def _roll_dice(number_of_dice) -> List[Die]:
-    dice: List[Die] = []
-
-    for _ in range(number_of_dice):
-        die: Die = Die()
-        die.roll()
-        dice.append(die)
-
-    return dice
-
-
 class Attack(object):
     def __init__(self, attacker: UnitProfile, target: UnitProfile, weapon: WeaponProfile):
         self._attacker = attacker
         self._target = target
         self._weapon = weapon
-        self._attacks: int = None
-        self._successful_attacks: int = None
+        self._hits: int = None
         self._wounds: int = None
-        self._successful_wounds: int = None
         self._saves: int = None
-        self._successful_saves: int = None
         self._damage: int = None
         self._hit_rolls: List[Die] = None
         self._wound_rolls: List[Die] = None
         self._save_rolls: List[Die] = None
 
-    def resolve(self):
-        self._attacks = self._attacker['models'] * self._weapon['attacks']
-        self._hit_rolls = _roll_dice(self._attacks)
-        self._successful_attacks = len([hit_roll for hit_roll in self._hit_rolls if self._is_successful_hit_roll(hit_roll)])
+    @property
+    def hits(self) -> int:
+        return self._hits
 
-        self._wounds = self._successful_attacks
-        self._wound_rolls = _roll_dice(self._wounds)
-        self._successful_wounds = len([wound_roll for wound_roll in self._wound_rolls if self._is_successful_wound_roll(wound_roll)])
+    @property
+    def wounds(self) -> int:
+        return self._wounds
 
-        self._saves = self._successful_wounds
-        self._save_rolls = _roll_dice(self._saves)
-        self._successful_saves = len([save_roll for save_roll in self._save_rolls if self._is_successful_save_roll(save_roll)])
+    @property
+    def saves(self) -> int:
+        return self._saves
 
-        self._damage = (self._successful_wounds - self._successful_saves) * self._weapon['damage']
+    @property
+    def damage(self) -> int:
+        return self._damage
 
-        self._print_resolution()
+    def resolve(self) -> None:
+        number_of_hit_rolls: int = self._attacker['models'] * self._weapon['attacks']
+        self._hit_rolls = roll_dice(number_of_hit_rolls)
+        self._hits = len([hit_roll for hit_roll in self._hit_rolls if self._is_successful_hit_roll(hit_roll)])
+
+        number_of_wound_rolls = self._hits
+        self._wound_rolls = roll_dice(number_of_wound_rolls)
+        self._wounds = len(
+            [wound_roll for wound_roll in self._wound_rolls if self._is_successful_wound_roll(wound_roll)])
+
+        number_of_save_rolls = self._wounds
+        self._save_rolls = roll_dice(number_of_save_rolls)
+        self._saves = len([save_roll for save_roll in self._save_rolls if self._is_successful_save_roll(save_roll)])
+
+        self._damage = (self._wounds - self._saves) * self._weapon['damage']
 
     def _is_successful_hit_roll(self, hit_roll) -> bool:
         return hit_roll.total >= self._attacker['ballistic_skill']
@@ -101,15 +110,34 @@ class Attack(object):
 
         return (save_roll.total - weapon_armour_penetration) >= target_save
 
-    def _print_resolution(self) -> None:
-        message = (
-            f"{self._attacks} attacks were made of which {self._successful_attacks} were successful.\n"
-            f"{self._wounds} wounds were made of which {self._successful_wounds} were successful.\n"
-            f"{self._saves} saves were made of which {self._successful_saves} were successful.\n"
-            f"The total damage inflicted was {self._damage}.\n"
-        )
 
-        print(message)
+def roll_dice(number_of_dice: int) -> List[Die]:
+    dice: List[Die] = []
+
+    for _ in range(number_of_dice):
+        die: Die = Die()
+        die.roll()
+        dice.append(die)
+
+    return dice
+
+
+def aggregate_attacks(attacks: List[Attack]) -> Result:
+    return {
+        'average_hits': statistics.mean([attack.hits for attack in attacks]),
+        'average_wounds': statistics.mean([attack.wounds for attack in attacks]),
+        'average_saves': statistics.mean([attack.saves for attack in attacks]),
+        'average_damage': statistics.mean([attack.damage for attack in attacks])
+    }
+
+
+def print_result(result: Result) -> None:
+    message: str = (
+        f"Average damage of {result['average_damage']}: "
+        f"{result['average_hits']} hits; {result['average_wounds']} wounds; {result['average_saves']} saves."
+    )
+
+    print(message)
 
 
 if __name__ == '__main__':
@@ -130,5 +158,13 @@ if __name__ == '__main__':
         'damage': 2
     }
 
-    attack: Attack = Attack(attacker, target, weapon)
-    attack.resolve()
+    attacks: List[Attack] = []
+
+    for i in range(10000):
+        attack: Attack = Attack(attacker, target, weapon)
+        attack.resolve()
+        attacks.append(attack)
+
+    result: Result = aggregate_attacks(attacks)
+
+    print_result(result)
